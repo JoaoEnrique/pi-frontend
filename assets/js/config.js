7e33049e56e907aa94bb4cd620e0c378754483c4
joaoenrique <jebsantosalves@gmail.com>
2024-10-12 13:34:30 -0300

    apagar curso, fim do CRUD de curso

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,7 +1,7 @@
 const API_URL = 'http://localhost:3000/api';
 
 const showEror = (title, text) => {
-    // document.querySelector(".alert-success").style.display = 'none';
+    document.querySelector(".alert-success").style.display = 'none';
     document.querySelector(".alert-danger").style.display = 'block';
     document.querySelector(".alert-danger #alert-title").innerHTML = title;
     document.querySelector(".alert-danger #alert-text").innerHTML = text;
--- assets/js/courses/update.js
+++ assets/js/courses/update.js
@@ -1,10 +1,15 @@
-import { API_URL, showEror, handleErrorSession } from "../config.js";
+import { API_URL, showEror, handleErrorSession, handleSuccessSession } from "../config.js";
 
 // Função para fazer a requisição
 async function index() {
     const currentUrl = window.location.href;
     const url = new URL(currentUrl);// Cria um objeto URL a partir da URL atual
     const params = new URLSearchParams(url.search); // Usa URLSearchParams para obter os parâmetros da query string
+
+    if(!params.get("id")){
+        handleErrorSession('Erro ao buscar curso:', "Esse curso não foi encontrado", 'curso.html');
+        return
+    }
     
     const response = await axios(`${API_URL}/courses/${params.get("id")}`);
 
@@ -15,6 +20,7 @@ async function index() {
 async function render() {
     try {
         const course = await index();
+        
         if(!course){
             handleErrorSession('Erro ao buscar curso:', "Esse curso não foi encontrado", 'curso.html');
             return
@@ -26,13 +32,39 @@ async function render() {
         document.querySelector("#period").value = course.period;
         document.querySelector("#type_work").value = course.type_work;
         
-        document.querySelector("#annual").checked = course.is_annual
-        document.querySelector("#bimonthly").checked = !course.is_annual
-        
+        // anual ou bimestral
+        document.querySelector("#bimonthly").checked = !course.is_annual;
+        document.querySelector("#annual").checked = course.is_annual;
+
     } catch (error) {
         showEror('Erro ao buscar curso:', error?.response?.data?.error ?  error?.response?.data?.error : error.message );
         console.error('Ocorreu um erro:', error);
     }
 }
+
+
+async function update(e){
+    try {
+        e.preventDefault();
+        const data = Object.fromEntries(new FormData(e.target).entries());
+
+        if(!data.id || !data.coordinator_id || !data.name || !data.period || !data.is_annual || !data.type_work){
+            showEror('Erro:', 'Informe todos os campos');
+            return;
+        }    
+
+        document.querySelector(".alert-danger").style.display = 'none';
+
+        const response = await axios.put(`${API_URL}/courses/${data.id}`, data);
+
+        handleSuccessSession(response.data.message, `edit_curso.html?id=${response.data.course.id}`);
+
+    } catch (error) {
+        showEror('Erro:', error?.response?.data?.error ?  error?.response?.data?.error : error.message );
+        console.error('Ocorreu um erro:', error);
+    }
+}
+
+document.querySelector("#form").addEventListener('submit', (e) => update(e))
 
 render(); //renderiza informações do curso
