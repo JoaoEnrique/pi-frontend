69439d03712d70c91b37af04076f14a73c9111f7
joaoenrique <jebsantosalves@gmail.com>
2024-10-14 09:56:27 -0300

    feat: cadastro de professor

---
+++ assets/js/teachers/store.js
@@ -0,0 +1,25 @@
+import { API_URL, showEror, handleSuccessSession } from "../config.js";
+
+async function store(e){
+    try {
+        e.preventDefault();
+        const data = Object.fromEntries(new FormData(e.target).entries());
+
+        if(!data.name || !data.email || !data.code){
+            showEror('Erro:', 'Informe todos os campos');
+            return;
+        }    
+
+        document.querySelector(".alert-danger").style.display = 'none';
+
+        const response = await axios.post(`${API_URL}/teachers`, data);
+
+        handleSuccessSession(response.data.message, `edit_professor.html?id=${response.data.teacher.id}`);
+
+    } catch (error) {
+        showEror('Erro:', error?.response?.data?.error ?  error?.response?.data?.error : error.message );
+        console.error('Ocorreu um erro:', error);
+    }
+}
+
+document.querySelector("#form").addEventListener('submit', (e) => store(e))
---
+++ assets/js/teachers/update.js
@@ -0,0 +1,65 @@
+import { API_URL, showEror, handleErrorSession, showSuccess } from "../config.js";
+
+// Função para fazer a requisição
+async function index() {
+    const currentUrl = window.location.href;
+    const url = new URL(currentUrl);// Cria um objeto URL a partir da URL atual
+    const params = new URLSearchParams(url.search); // Usa URLSearchParams para obter os parâmetros da query string
+
+    if(!params.get("id")){
+        handleErrorSession('Erro ao buscar professor:', "Esse professor não foi encontrado", 'professors.html');
+        return
+    }
+    
+    const response = await axios(`${API_URL}/teachers/${params.get("id")}`);
+
+    return await response.data;
+}
+
+// apresenta conteudo na tela
+async function render() {
+    try {
+        const student = await index();
+        
+        if(!student){
+            handleErrorSession('Erro ao buscar professor:', "Esse professor não foi encontrado", 'professors.html');
+            return
+        }
+
+        document.querySelector("#id").value = student.id;
+        document.querySelector("#name").value = student.name;
+        document.querySelector("#email").value = student.email;
+        document.querySelector("#code").value = student.code;
+
+    } catch (error) {
+        showEror('Erro ao buscar curso:', error?.response?.data?.error ?  error?.response?.data?.error : error.message );
+        console.error('Ocorreu um erro:', error);
+    }
+}
+
+
+async function update(e){
+    try {
+        e.preventDefault();
+        const data = Object.fromEntries(new FormData(e.target).entries());
+
+        if(!data.name || !data.email || !data.code){
+            showEror('Erro:', 'Informe todos os campos');
+            return;
+        }    
+
+        document.querySelector(".alert-danger").style.display = 'none';
+
+        const response = await axios.put(`${API_URL}/teachers/${data.id}`, data);
+
+        showSuccess(response.data.message);
+
+    } catch (error) {
+        showEror('Erro:', error?.response?.data?.error ?  error?.response?.data?.error : error.message );
+        console.error('Ocorreu um erro:', error);
+    }
+}
+
+document.querySelector("#form").addEventListener('submit', (e) => update(e))
+
+render(); //renderiza informações do no form
