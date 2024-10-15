dab7f203ee86c2b6d9eda8bad5441f55a44f514b
joaoenrique <jebsantosalves@gmail.com>
2024-10-15 19:36:23 -0300

    feat: home do professor

--- src/Pages/HomeTeacher/index.tsx
+++ src/Pages/HomeTeacher/index.tsx
@@ -1,3 +1,45 @@
+import { Card } from "../../components/Card";
+import { Footer } from "../../components/Footer";
+import { Nav } from "../../components/Nav";
+
 export function HomeTeacher(){
-    return <h1></h1>
+    return (
+        <>
+            <Nav/>
+            <div className="container">
+                <h1>Bem-vindo, professor Antônio</h1>
+                <p>O que deseja realizar?</p>
+                <div className="action-buttons">
+                    <Card
+                        link="/professor/dashboard"
+                        imgSrc="/img/grupo.png"
+                        imgAlt="GRUPOS"
+                        title="GRUPOS"
+                        description="Gerencie o trabalho das suas turmas"
+                    />
+                    
+                    <Card
+                        link="/coordenador/dashboard"
+                        imgSrc="/img/aptplan.png"
+                        imgAlt="RELATÓRIO"
+                        title="RELATÓRIO"
+                        description="Acompanhe o relatório de andamento dos trabalhos"
+                    />
+                    
+                    <Card
+                        link="/adm/dashboard"
+                        imgSrc="/img/alunos.png"
+                        imgAlt="ALUNOS"
+                        title="ALUNOS"
+                        description="Verifique seus alunos"
+                    />
+                </div>
+                <div className="main-image">
+                    <img src="/img/illustration.png" alt="Ilustração"/>
+                </div>
+                <p>Gerencie trabalhos de graduação de onde você estiver!</p>
+            </div>
+            <Footer/>
+        </>
+    )
 }
--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,12 +1,13 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Index } from "./Pages/Index";
+import { HomeTeacher } from "./Pages/HomeTeacher";
 
 export function AppRoutes(){
     return (
         <Router>
             <Routes>
                 <Route path="/" element={ <Index/> }/>
-                <Route path="/professor/dashboard" element={ <Index/> }/>
+                <Route path="/professor/dashboard" element={ <HomeTeacher/> }/>
             </Routes>
         </Router>
     );
